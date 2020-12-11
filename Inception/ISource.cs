d6673d442d12a7c4fbe984c16a65532eb6ff90b6
know <do.i.know.it@yggdrashill.com>
2020-12-11 18:55:54 +0900

    Add definition for Inception

---
+++ Inception/ISource.cs
@@ -0,0 +1,13 @@
+﻿using YggdrAshill.Heimdallr.Elucidation;
+
+namespace YggdrAshill.Heimdallr.Inception
+{
+    public interface ISource<TItem> :
+        IObservation<TItem>,
+        IUnsubscription,
+        IOrigination
+        where TItem : IItem
+    {
+
+    }
+}
