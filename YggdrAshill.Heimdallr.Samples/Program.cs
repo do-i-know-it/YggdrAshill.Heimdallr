64790277ad0e93f0c0495633cc2c861f3dec2978
know <do.i.know.it@yggdrashill.com>
2021-07-18 17:42:47 +0900

    Add sample for framework

---
+++ YggdrAshill.Heimdallr.Samples/Program.cs
@@ -0,0 +1,29 @@
+using YggdrAshill.Heimdallr.Historization;
+using System;
+
+namespace YggdrAshill.Heimdallr.Samples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var indication = new WriteLogToConsole();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter any key to emit signals.");
+                Console.WriteLine("Please enter \"q\" to quit this program.");
+                Console.Write("Key: ");
+
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    Console.WriteLine("quit.");
+                    return;
+                }
+
+                indication.Record(Log.Severity.Information, input);
+            }
+        }
+    }
+}
